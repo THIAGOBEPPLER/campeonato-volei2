390193b007b119e3ac7c5648ffc28cba3d90d5e4
thiago <thiagobeppler@hotmail.com>
2022-06-20 15:32:59 -0300

    Initial commit

---
+++ src/main/kotlin/com/api/campeonatovolei2/CampeonatoVolei2Application.kt
@@ -0,0 +1,11 @@
+package com.api.campeonatovolei2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CampeonatoVolei2Application
+
+fun main(args: Array<String>) {
+	runApplication<CampeonatoVolei2Application>(*args)
+}
---
+++ src/test/kotlin/com/api/campeonatovolei2/CampeonatoVolei2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.api.campeonatovolei2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CampeonatoVolei2ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
